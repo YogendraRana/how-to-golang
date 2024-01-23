36008e02964270cc59350e277b6f34a9415a59bf
Yogendra Rana <yogendrarana.tech@gmail.com>
2024-01-23 18:59:24 +0545

    Updated the example of interface

--- interface/interface.go
+++ interface/interface.go
@@ -2,64 +2,41 @@ package main
 
 import "fmt"
 
-// interface 1
-type PersonInterface interface {
-	GetName() string
-	GetAge() int
-}
-
-// struct 1
-type PersonStruct struct {
-	Name string
-	Age  int
-}
-
-// implement interface 1
-func (person PersonStruct) GetName() string {
-	return person.Name
-}
-
-func (person PersonStruct) GetAge() int {
-	return person.Age
-}
-
-// interface 2
+// Define an interface named Shape
 type Shape interface {
 	Area() float64
-	Perimeter() float64
 }
 
-// struct 2
+// Implement the Shape interface for a rectangle
 type Rectangle struct {
 	Width  float64
 	Height float64
 }
 
-// implement interface 2
 func (r Rectangle) Area() float64 {
 	return r.Width * r.Height
 }
 
-func (r Rectangle) Perimeter() float64 {
-	return 2*r.Width + 2*r.Height
+// Implement the Shape interface for a circle
+type Circle struct {
+	Radius float64
+}
+
+func (c Circle) Area() float64 {
+	return 3.14 * c.Radius * c.Radius
+}
+
+// Function that takes any Shape and calculates its area
+func PrintArea(s Shape) {
+	fmt.Printf("Area of the shape: %f\n", s.Area())
 }
 
 func main() {
-	var Alex PersonInterface = PersonStruct{
-		Name: "Alex",
-		Age:  20,
-	}
+	// Create instances of Rectangle and Circle
+	rectangle := Rectangle{Width: 5, Height: 3}
+	circle := Circle{Radius: 2}
 
-	fmt.Printf("Name of alex is obviously %v\n", Alex.GetName())
-	fmt.Printf("Age of alex is %v\n", Alex.GetAge())
-
-	var shape Shape
-	shape = Rectangle{Width: 4, Height: 5}
-
-	area := shape.Area()
-	perimeter := shape.Perimeter()
-
-	fmt.Printf("Area of the rectangle is %v\n", area)
-	fmt.Printf("Perimeter of the rectangle is %v\n", perimeter)
-
+	// Use the PrintArea function with different shapes
+	PrintArea(rectangle)
+	PrintArea(circle)
 }
