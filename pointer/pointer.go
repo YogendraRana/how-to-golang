2bd5be95777125eed2ef17fc137d6eda60af96f6
Yogendra Rana <yogendrarana.tech@gmail.com>
2024-01-23 16:59:49 +0545

    Updated the pointer example

--- pointer/pointer.go
+++ pointer/pointer.go
@@ -2,18 +2,27 @@ package main
 
 import "fmt"
 
-func printX(x int) {
-	fmt.Println(x)
-}
-
-func printY(y *int) {
-	fmt.Println(*y)
-}
-
 func main() {
-	var x int = 10
-	var y int = 50
+	// Declare a variable
+	var number int = 42
 
-	printX(x)
-	printY(&y)
+	// Declare a pointer variable
+	var pointerToNumber *int
+
+	// Assign the address of the variable to the pointer
+	pointerToNumber = &number
+
+	// Print the value and address of the variable
+	fmt.Println("Value of number:", number)
+	fmt.Println("Address of number:", &number)
+
+	// Print the value and address stored in the pointer
+	fmt.Println("Value pointed by pointerToNumber:", *pointerToNumber)
+	fmt.Println("Address stored in pointerToNumber:", pointerToNumber)
+
+	// Modify the value indirectly through the pointer
+	*pointerToNumber = 100
+
+	// Print the modified value of the variable
+	fmt.Println("Modified value of number:", number)
 }
